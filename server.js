7a6de02f7fa27ad12834e0d8cb32c138ff57b66b
Orhan Taghizadeh <43483015+tagizadeorxan@users.noreply.github.com>
2020-11-30 23:59:01 +0400

    cors

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const mysql = require("mysql");
 const app = express();
 require('dotenv').config();
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 // parse requests of content-type: application/json
 app.use(bodyParser.json());
 
